bb9e8f70251a1ea4a03ea45a8f1f73553397d348
LaZBeyi <89390663+LaZBeyi@users.noreply.github.com>
2024-04-17 10:11:31 +0300

    LazBeyi.sh

---
+++ LazBeyi.sh
@@ -0,0 +1,47 @@
+apt install figlet
+
+clear
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+
+echo -e "$BBlue Dostum olmaz,hasmım yasamaz!"  
+echo -e "$BYellow :::::::::::::::::::::::::::::::::::::::::::::::::::::::"  
+echo -e "$BGreen >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"  
+echo -e "$BBlue <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"  
+echo -e "$BYellow :::::::::::::::::::::::::::::::::::::::::::::::::::::::"  
+echo -e "$BRed dostum olmaz,hasmım yasamaz!"  
+echo -e "$BGreen dostum olmaz,hasmım yasamaz!"  
+echo -e "$BBlue :::::::::::::::::::::::::::::::::::::::::::::::::::::::"  
+echo -e "$BYellow >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"  
+echo -e "$BRed <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"  
+echo -e "$BGreen :::::::::::::::::::::::::::::::::::::::::::::::::::::::"  
+echo -e "$BBlue dostum olmaz,hasmım yasamaz!"  
+echo -e "             $Green Create by :- Laz Beyi"
+echo
+
+#dtls Input
+echo -e "$Blue yazıyı gir cano"
+read varC
+
+figlet $varC 
+
+echo
+echo -e "$BYellow _______________________________________"
+echo
+echo -e "$Red toolu kullandığın için sagol cano"
+
+exit
